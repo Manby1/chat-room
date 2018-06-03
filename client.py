c3597a6e3ee3d03624b10f4293fad1e013499991
Zeitocrab <30536860+Zeitocrab@users.noreply.github.com>
2018-06-03 21:10:07 +0100

    Create base sockets for client and server\n\ntest description

--- client.py
+++ client.py
@@ -1,15 +1,3 @@
-import asyncio
-import websockets
+import socket
 
-async def hello():
-    async with websockets.connect(
-            'ws://localhost:8765') as websocket:
-        name = input("What's your name? ")
-
-        await websocket.send(name)
-        print(f"> {name}")
-
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
-
-asyncio.get_event_loop().run_until_complete(hello())
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- server.py
+++ server.py
@@ -1,1 +1,3 @@
-import websockets
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
