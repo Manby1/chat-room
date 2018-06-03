fe6a636441de02c216f2fd4bd6e89fffd592787d
Kofi <gallopoldhorsie@outlook.com>
2018-06-03 21:27:37 +0100

    int

--- client/client.py
+++ client/client.py
@@ -5,7 +5,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #input ip and port
 IP_address = input("Enter IP Address to connect to:")
-port = input("Enter Port to connect to:")
+port = int(input("Enter Port to connect to:"))
 
 #connect to ip and port
 client.connect((IP_address, port))
--- server/server.py
+++ server/server.py
@@ -4,7 +4,7 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #input ip and port
 IP_address = input("Enter IP Address to connect to:")
-port = input("Enter Port to connect to:")
+port = int(input("Enter Port to connect to:"))
 
 #connect to ip and port
 server.bind((IP_address, port))
