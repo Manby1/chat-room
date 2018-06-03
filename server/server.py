bd10798f46e52b2ff4432a626ce697d5d0d1ba8a
Zeitocrab <30536860+Zeitocrab@users.noreply.github.com>
2018-06-03 21:25:31 +0100

    Server socket

--- server/server.py
+++ server/server.py
@@ -1,3 +1,10 @@
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#input ip and port
+IP_address = input("Enter IP Address to connect to:")
+port = input("Enter Port to connect to:")
+
+#connect to ip and port
+server.bind((IP_address, port))
