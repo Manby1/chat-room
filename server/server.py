094cb37770d1926b1b834efbb8d8f16266bee8ac
Kofi <gallopoldhorsie@outlook.com>
2018-06-05 22:09:37 +0100

    were getting somewhere....

--- server/server.py
+++ server/server.py
@@ -2,9 +2,8 @@ import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#input ip and port
-IP_address = input("Enter IP Address to connect to:")
-port = int(input("Enter Port to connect to:"))
+IP_address = 192.168.1.117
+port = 843
 
-#connect to ip and port
+#setup using ip and port
 server.bind((IP_address, port))
