90f61983c3e435c19c092555f7aaa3af17268f28
Kofi <gallopoldhorsie@outlook.com>
2018-06-06 18:51:25 +0100

    Test

--- server/server.py
+++ server/server.py
@@ -2,11 +2,11 @@ import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#IP_address = input('IP: ')
+IP_address = input('IP: ')
 port = int(input('Port: '))
 
 #setup using ip and port
-server.bind((socket.gethostname(), port))
+server.bind((IP_address, port))
 server.listen(2)
 
 while True:
