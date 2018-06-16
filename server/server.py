6521c7b224f257278a1ef090b8479dd0805bfec2
Zeitocrab <30536860+Zeitocrab@users.noreply.github.com>
2018-06-16 18:32:21 +0100

    added ASYNCIO!!!

--- server/server.py
+++ server/server.py
@@ -1,35 +1,41 @@
-import socket
+import socket, asyncio
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+loop = asyncio.get_event_loop()
 
+#automatic connection
 with open('../server.txt') as f:
     data = f.read()
 IP_address = data[0:13]
 Port = int(data[14:17])
-
-'''
-IP_address = input('IP: ')
-port = int(input('Port: '))
-'''
-
-#address list
+#address list - names
 addresses = {}
 
-#setup using ip and port
-server.bind((IP_address, Port))
-server.listen(5)
-print('Server listening...')
+#manual connection
+'''
+IP_address = input('IP: ')
+Port = int(input('Port: '))
+'''
 
-while True:
-    try:
-        (client_socket, address) = server.accept()
-        print('New connection:', address)
-        while True:
-            data = str(client_socket.recv(512))[2:-1]
-            if not address in addresses:
-                addresses[address] = data
-            print(addresses[address], data)
+async def serverLoop(address, port, connections):
+    #setup using ip and port
+    server.bind((address, port))
+    server.listen(connections)
+    print('Server listening...')
+    while True:
+        try:
+            (client_socket, address) = server.accept()
+            print('New connection:', address)
+            while True:
+                data = str(client_socket.recv(512))[2:-1]
+                if not address in addresses:
+                    addresses[address] = data
+                print(addresses[address], data)
 
-    except Exception as e:
-        print(e)
-        server.close()
+        except Exception as e:
+            print(e)
+            server.close()
+
+loop.create_task(serverLoop(5, IP_address, Port, 5))
+loop.run_forever()
+loop.close()
