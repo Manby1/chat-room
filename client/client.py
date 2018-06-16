a8012ef05eb9be2f8c6921b9185d31af51affdcb
Kofi <gallopoldhorsie@outlook.com>
2018-06-16 19:37:10 +0100

    comments client.py

--- client/client.py
+++ client/client.py
@@ -1,20 +1,32 @@
 import socket
 
+#creates client socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+#automatic connection
 with open('../server.txt') as f:
     data = f.read()
 IP_address = data[0:13]
 Port = int(data[14:17])
 
+#manual connection
+'''
+IP_address = input('IP: ')
+Port = int(input('Port: '))
+'''
+
+#message encoder
 commands = {'message':'m|', 'name':'n|'}
 def send(message, type):
     client.send(bytes(commands[type]+str(message), 'utf-8'))
 
+#nickname
 name = input('Name?\n')
 
 #connect to ip and port
 client.connect((IP_address, Port))
+
+#sends name
 send(name, 'name')
 
 while True:
