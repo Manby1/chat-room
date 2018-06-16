00a4e9bc4694bb8387d1956f94f049a5a04ed69b
Kofi <gallopoldhorsie@outlook.com>
2018-06-16 10:21:57 +0100

    reads ip info from my local file

--- client/client.py
+++ client/client.py
@@ -1,10 +1,16 @@
 import socket
 
-#set up client socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+f = open('server.txt')
+file = f.read()
+IP_address = file[0:13]
+port = int(file[14:17])
+
+'''
 IP_address = input('IP: ')
 port = int(input('Port: '))
+'''
 
 #connect to ip and port
 client.connect((IP_address, port))
