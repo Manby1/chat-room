75246b66b26c0a5f33cc705798a8cb92841d44f2
Kofi <gallopoldhorsie@outlook.com>
2018-06-23 00:17:42 +0100

    color

--- client/tkClient.py
+++ client/tkClient.py
@@ -40,7 +40,6 @@ async def main():
 async def update():
     while True:
         received = receive()
-        print(received)
         if not received == None:
             textList.addItem(received[1], received[0])
             textList.print()
