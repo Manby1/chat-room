ff8f22affdcebf96cb0a1d40cc70f68078fcb06f
Zeitocrab <30536860+Zeitocrab@users.noreply.github.com>
2018-06-24 14:04:04 +0100

    start up uno

---
+++ uno/server.py
@@ -0,0 +1,110 @@
+import socket, asyncio, json, random
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+loop = asyncio.get_event_loop()
+
+#automatic connection
+with open('../server.txt') as f:
+    data = f.read()
+IP_address = data[0:13]
+Port = int(data[14:17])
+#client objects
+clients = {}
+
+class client:
+    def __init__(self, socket):
+        self.socket = socket
+        color = str(hex(random.randint(0, 256**3-1)))[2:]
+        self.color = '#'+'0'*(6-len(color))+color
+        self.name = None
+    def setName(self, name):
+        self.name = name
+    def send(self, message):
+        self.socket.send(bytes(json.dumps(message), 'utf-8'))
+    def receive(self):
+        try:
+            msgs = split(self.socket.recv(512).decode())
+            return list(map(lambda msg: json.loads(msg), msgs))
+        except socket.timeout:
+            return []
+
+def split(text):
+    items = []
+    string = ''
+    for i in text:
+        string += i
+        if i == ']':
+            items.append(string)
+            string = ''
+    return items
+
+#manual connection
+'''
+IP_address = input('IP: ')
+Port = int(input('Port: '))
+'''
+
+def sendToAll(color, message):
+    for client in clients:
+        client.send((color, message))
+
+#individual socket handler
+async def connection(client_socket, address):
+    me = client(client_socket)
+    clients[me] = me
+    print('Now listening to connection:', address)
+    clients[me].socket.settimeout(0.5)
+    while me in clients:
+        try:
+            data = clients[me].receive()
+            #recieve and interpret data
+            if not data == []:
+                for msg in data:
+                    type = msg[0]
+                    message = msg[1]
+                    #rename command
+                    if type == 'n':
+                        if clients[me].name == None:
+                            output = message+' has connected!'
+                            print(output)
+                            sendToAll(clients[me].color, output)
+                        else:
+                            output = clients[me].name+' has changed their name to '+message+'!'
+                            print(output)
+                            sendToAll(clients[me].color, output)
+                        clients[me].name = message
+
+                    #plain message
+                    elif type == 'm':
+                        output = clients[me].name+': '+message
+                        print(output)
+                        sendToAll(clients[me].color, output)
+        except ConnectionResetError:
+            output = clients[me].name+' left...'
+            print(output)
+            color = clients[me].color
+            clients.pop(clients[me])
+            sendToAll(color, output)
+        await asyncio.sleep(0.1)
+    print('Client loop ended.')
+
+#searches for new clients and allocates them their own loop
+async def serverLoop(address, port, connections):
+    # setup using ip and port
+    server.bind((address, port))
+    server.listen(connections)
+    server.settimeout(1)
+    print('Server listening...')
+
+    while True:
+        try:
+            (client_socket, address) = server.accept()
+            print("Received an address!")
+            asyncio.ensure_future(connection(client_socket, address))
+        except socket.timeout:
+            pass
+        await asyncio.sleep(0.1)
+
+loop.create_task(serverLoop(IP_address, Port, 5))
+loop.run_forever()
+loop.close()
---
+++ uno/tkClient.py
@@ -0,0 +1,129 @@
+import socket, json, asyncio, tkinter, sys
+
+class messageList:
+    def __init__(self, length):
+        self.length = length
+        self.items = [''] * length
+        self.labels = [None] * length
+    def addItem(self, text, color):
+        self.items.pop(0)
+        self.items.append(text)
+        self.labels.pop(0)
+        self.labels.append(tkinter.Label(root, text=text, font=("arial", 10, "bold"), fg=color))
+    def print(self):
+        for i in range(self.length):
+            if not self.labels[i] == None:
+                self.labels[i].place(x=10, y=i*20+10)
+
+def boxSend():
+    if not message.get() == '':
+        send('m', message.get())
+        box.delete(0, 'end')
+
+textList = messageList(12)
+
+root = tkinter.Tk()
+root.title("Chatroom")
+root.geometry("400x300")
+window = tkinter.PanedWindow(root)
+message = tkinter.StringVar()
+if sys.platform == 'darwin':
+    #mac
+    box = tkinter.Entry(root, textvariable=message, width=32, bg="white")
+    box.place(x=10, y = 270)
+    box.bind('<Return>', lambda event: boxSend())
+    tkinter.Button(root, text="Send", width=5, height=1, bg="grey", command=boxSend).place(x=323, y=270)
+else:
+    #w-windows?
+    box = tkinter.Entry(root, textvariable=message, width=50, bg="white")
+    box.place(x=10, y = 270)
+    box.bind('<Return>', lambda event: boxSend())
+    tkinter.Button(root, text="Send", width=5, height=1, bg="grey", command=boxSend).place(x=335, y=266)
+
+async def main():
+    running = True
+    while running:
+        try:
+            root.update()
+            await asyncio.sleep(0.01)
+        except tkinter.TclError:
+            running = False
+    client.close()
+    loop.close()
+    quit()
+
+async def update():
+    while True:
+        received = receive()
+        if not received == []:
+            for msg in received:
+                textList.addItem(msg[1], msg[0])
+            textList.print()
+        await asyncio.sleep(0.01)
+
+def send(type, message):
+    print(type, message)
+    client.send(bytes(json.dumps((type, message)), 'utf-8'))
+
+def receive():
+    try:
+        msgs = split(client.recv(512).decode())
+        return list(map(lambda msg: json.loads(msg), msgs))
+    except socket.timeout:
+        return []
+
+def split(text):
+    items = []
+    string = ''
+    for i in text:
+        string += i
+        if i == ']':
+            items.append(string)
+            string = ''
+    return items
+
+#creates client socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+#automatic connection
+with open('../server.txt') as f:
+    data = f.read()
+IP_address = data[0:13]
+Port = int(data[14:17])
+
+#manual connection
+'''
+IP_address = input('IP: ')
+Port = int(input('Port: '))
+'''
+
+#connect to ip and port
+trying = True
+while trying:
+    print('Searching for server...')
+    try:
+        client.connect((IP_address, Port))
+        print('Connected!')
+        trying = False
+    except ConnectionRefusedError:
+        print('No server found.')
+        if not input('Try again? (Y/N)\n').upper() == 'Y':
+            trying = False
+            quit()
+
+client.settimeout(0.1)
+
+#sends nickname
+name = input('Name: ')
+send('n', name)
+
+
+
+loop = asyncio.get_event_loop()
+loop.create_task(main())
+loop.create_task(update())
+loop.run_forever()
+loop.close()
+
+root.mainloop()
