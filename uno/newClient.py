db005344d5494116b7a93bb02196ddb1e44311c4
Kofi <gallopoldhorsie@outlook.com>
2018-06-30 14:31:35 +0100

    Keys

--- uno/newClient.py
+++ uno/newClient.py
@@ -102,11 +102,14 @@ class Screen:
         #play widgets
         play_host = Button((300, 400), 'Host', 55, colour=(0, 0, 0), font_colour=(255, 255, 255), border_size=30, border_colour=(50, 0, 0), width = 240, height = 150)
         play_join = Button((700, 400), 'Join', 55, colour=(0, 255, 0), font_colour=(0, 0, 0), border_size=30, border_colour = (0, 200, 0), width = 240, height = 150)
-        play_back = Button((70, 50), 'Back', 20, colour=(0, 0, 0), font_colour=(255, 255, 255))
+
+        #back
+        back = Button((70, 50), 'Back', 20, colour=(0, 0, 0), font_colour=(255, 255, 255))
 
         #list of screens and their widgets
         self.screens = {'title':{'play':title_play, 'quit':title_quit, 'splash':title_splash},
-                        'play':{'host':play_host, 'join':play_join, 'back':play_back}}
+                        'play':{'host':play_host, 'join':play_join, 'back':back},
+                        'join':{'back':back}}
 
     def switchScreen(self, screen):
         self.active_widgets = self.screens[screen]
@@ -120,6 +123,11 @@ class Screen:
     def play(self):
         self.switchScreen('play')
         display.fill((255, 0, 0))
+        screen.print()
+
+    def join(self):
+        self.switchScreen('join')
+        display.fill((255, 255, 0))
         screen.print()
 
     def print(self):
@@ -151,6 +159,14 @@ class Image:
     def print(self):
         display.blit(self.image, self.image_rect)
 
+pygameKeys = {pygame.K_0:'0', pygame.K_1:'1', pygame.K_2:'2', pygame.K_3:'3', pygame.K_4:'4', pygame.K_5:'5',
+              pygame.K_6:'6', pygame.K_7:'7', pygame.K_8:'8', pygame.K_9:'9', pygame.K_a:'a', pygame.K_b:'b',
+              pygame.K_c:'c', pygame.K_d:'d', pygame.K_e:'e', pygame.K_f:'f', pygame.K_g:'g', pygame.K_h:'h',
+              pygame.K_i:'i', pygame.K_j:'j', pygame.K_k:'k', pygame.K_l:'l', pygame.K_m:'m', pygame.K_n:'n',
+              pygame.K_o:'o', pygame.K_p:'p', pygame.K_q:'q', pygame.K_r:'r', pygame.K_s:'s', pygame.K_t:'t',
+              pygame.K_u:'u', pygame.K_v:'v', pygame.K_w:'w', pygame.K_x:'x', pygame.K_y:'y', pygame.K_z:'z',
+              pygame.K_SPACE:' ', pygame.K_PERIOD:'.', pygame.K_SEMICOLON:';', pygame.K_SLASH:'/', pygame.K_COMMA:',',
+              pygame.K_HASH:'#', pygame.K_QUOTE:"'"}
 
 #mouse
 mouse = Mouse()
@@ -162,7 +178,7 @@ screen = Screen()
 screen.title()
 
 while True:
-    pygame.event.get()
+    events = pygame.event.get()
 
     #title screen loop
     if screen.current_screen == 'title':
@@ -172,14 +188,21 @@ while True:
             pygame.quit()
             quit()
 
-    #next screen loop
+    #play screen loop
     elif screen.current_screen == 'play':
         if mouse.click(screen.getWidget('play', 'host')):
             print('Host')
         elif mouse.click(screen.getWidget('play', 'join')):
-            print('Join')
+            screen.join()
         elif mouse.click(screen.getWidget('play', 'back')):
             screen.title()
+
+    elif screen.current_screen == 'join':
+        for event in events:
+            if event.type == pygame.KEYDOWN and event.key in pygameKeys:
+                print(pygameKeys[event.key])
+        if mouse.click(screen.getWidget('join', 'back')):
+            screen.play()
 
     pygame.display.update()
     mouse.update()
