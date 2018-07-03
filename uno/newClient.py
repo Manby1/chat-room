ee658d075ebf7ff129e6a7f734ac8f835bea2672
Kofi <gallopoldhorsie@outlook.com>
2018-07-03 20:47:50 +0100

    Player Class

--- uno/newClient.py
+++ uno/newClient.py
@@ -496,6 +496,17 @@ class Client(socket.socket):
                 
             await asyncio.sleep(0.1)
 
+players = []
+#Player
+class Player:
+    def __init__(self, slot):
+        self.name = None
+        self.avatar = None
+        self.is_host = False
+        self.slot = None
+    def allocate(self, slot):
+        self.slot = slot
+
 def compress(rgbarray):
     r = []
     for i in rgbarray[:-1]:
@@ -507,14 +518,16 @@ def compress(rgbarray):
 def decompress(rgbvalue):
     return (int(rgbvalue[:3]),int(rgbvalue[3:6]),int(rgbvalue[6:]),255)
 
-pygame_keys = {pygame.K_0:'0', pygame.K_1:'1', pygame.K_2:'2', pygame.K_3:'3', pygame.K_4:'4', pygame.K_5:'5',
+pygame_keys = {
+              pygame.K_0:'0', pygame.K_1:'1', pygame.K_2:'2', pygame.K_3:'3', pygame.K_4:'4', pygame.K_5:'5',
               pygame.K_6:'6', pygame.K_7:'7', pygame.K_8:'8', pygame.K_9:'9', pygame.K_a:'a', pygame.K_b:'b',
               pygame.K_c:'c', pygame.K_d:'d', pygame.K_e:'e', pygame.K_f:'f', pygame.K_g:'g', pygame.K_h:'h',
               pygame.K_i:'i', pygame.K_j:'j', pygame.K_k:'k', pygame.K_l:'l', pygame.K_m:'m', pygame.K_n:'n',
               pygame.K_o:'o', pygame.K_p:'p', pygame.K_q:'q', pygame.K_r:'r', pygame.K_s:'s', pygame.K_t:'t',
               pygame.K_u:'u', pygame.K_v:'v', pygame.K_w:'w', pygame.K_x:'x', pygame.K_y:'y', pygame.K_z:'z',
               pygame.K_SPACE:' ', pygame.K_PERIOD:'.', pygame.K_SEMICOLON:';', pygame.K_SLASH:'/', pygame.K_COMMA:',',
-              pygame.K_HASH:'#', pygame.K_QUOTE:"'"}
+              pygame.K_HASH:'#', pygame.K_QUOTE:"'"
+              }
 
 #mouse
 mouse = Mouse()
